b4fd84446489702be8f754c3f5479a04b0b02795
Cian Kelly <cian.kelly2@ucdconnect.ie>
2021-05-01 22:20:03 +0100

    Changing data storage type to struct

--- softwareengineering2.c
+++ softwareengineering2.c
@@ -1,84 +1,74 @@
-#include <stdio.h>                                                              
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 #define MAX_SIZE 20
 
-int start(char player1[MAX_SIZE], char player2[MAX_SIZE], int player1score, int player2score);
+struct board {
+  char board[8][8];
+  char player1[20];
+  char player2[20];
+  int player1score;
+  int player2score;
+};
+
+int start(struct board game);
 
 int main(void)
 {
-  char player1[20];
-  char player2[20];
-  char board[17][33];
-  int player1score=2;
-  int player2score=2;
-  int i=0;
-  int j=0;
-  int count=1;
-  char letter[8]={'a','b','c','d','e','f','g','h'};
-  //printf("%c", letter[0]);
+  struct board game;
+  game.player1score=2;
+  game.player2score=2;
+  //char player1[20];
+  //char player2[20];
+  //char board[8][8];
+  //int player1score=2;
+  //int player2score=2;
+  //int i=0;
+  //int j=0;
+  //int count=1;
+  //char letter[8]={'a','b','c','d','e','f','g','h'};
 
-  start(player1, player2, player1score, player2score);
-
-  for (i=0;i<17;i++)
-  {
-    if (i%2 == 0)
-    {
-      for (j=0;j<33;j++)
-      {
-        board[i][j] = '-';
-      }
-    }
-      else
-      {
-        for(j=0;j<33;j++)
-        {
-          if (j == 0 || j%4==0)
-          {
-            board[i][j] = '|';
-          }
-            else
-            {
-              board[i][j] = ' ';
-            }
-        }
-      }
-  }
-  board[7][14]='W';
-  board[7][18]='B';
-  board[9][14]='B';
-  board[9][18]='W';
-  printf("\n\n");
-  for (i=0;i<17;i++)
-  {
-    printf("\t ");
-    if (i % 2 != 0)
-    {
-      printf(" %d ", count);
-      count++;
-    }
-      else
-      {
-        printf("   ");
-      }
-    for (j=0;j<33;j++)
-    {
-      printf("%c", board[i][j]);
-    }
-    printf("\n");
-  }
-  printf("\t      a   b   c   d   e   f   g   h");
+  start(game);
 }
 
-int start(char player1[MAX_SIZE], char player2[MAX_SIZE], int player1score, int player2score)
+int start(struct board game)
 {
+  int i=0;
+  int j=0;
+  int counter=1;
+
+  for (i=0;i<8;i++)
+  {
+    for(j=0;j<8;j++)
+    {
+      game.board[i][j]='-';
+    }
+  }
+
+  game.board[3][3]='W';
+  game.board[3][4]='B';
+  game.board[4][3]='B';
+  game.board[4][4]='W';
   printf("-------------------------\n   Welcome to Othello !\n-------------------------\n");
   printf("\nEnter name of Player 1 (Black):\n");
-  scanf("%s", player1);
+  scanf("%s", game.player1);
   printf("\nEnter name of Player 2 (White):\n");
-  scanf("%s", player2);
-  printf("\n\nPlayer 1 : %s\nPlayer 2 : %s", player1, player2);
+  scanf("%s", game.player2);
+  printf("\n\nPlayer 1 : %s\nPlayer 2 : %s", game.player1, game.player2);
   //printf("\n\nScore: %s (Black) %d:%d %s (White)\n", player1, player1score, player2, player2score);
-  printf("\n\n\t   Score: %s (Black) %d : %d %s (White)", player1, player1score, player2score, player2);
+  printf("\n\n\t   Score: %s (Black) %d : %d %s (White)\n\n", game.player1, game.player1score, game.player2score, game.player2);
+
+
+  for (i=0;i<8;i++)
+  {
+    printf("\t\t      %d ", counter);
+    for(j=0;j<8;j++)
+    {
+      printf("%c ", game.board[i][j]);
+    }
+    printf("\n");
+    counter++;
+  }
+  printf("\t\t        a b c d e f g h\n");
 }
