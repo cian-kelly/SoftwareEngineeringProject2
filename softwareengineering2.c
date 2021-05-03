2c418b5a98adeca874a134f35331def4492a2330
Cian Kelly <cian.kelly2@ucdconnect.ie>
2021-05-03 17:36:05 +0100

    Trying to implement checker

--- softwareengineering2.c
+++ softwareengineering2.c
@@ -10,9 +10,13 @@ struct board {
   char player2[20];
   int player1score;
   int player2score;
+  char turn;
 };
 
 int start(struct board game);
+int gametracker(struct board game);
+int xconverter(char xcoordinate, int xcoordinateconverted);
+int movechecker(struct board game, int xcoordinateconverted, int ycoordinate);
 
 int main(void)
 {
@@ -71,4 +75,182 @@ int start(struct board game)
     counter++;
   }
   printf("\t\t        a b c d e f g h\n");
+}
+
+int gametracker(struct board game)
+{
+  bool started=false;
+  char userinput;
+  char xcoordinate;
+  int ycoordinate;
+  int xcoordinateconverted=10;
+  // 0 is black
+  game.turn='B';
+
+
+  if (game.turn == 'B')
+  {
+    if (!started)
+    {
+      printf("%s (Black) will start\n", game.player1);
+    }
+     else
+     {
+       printf("%s is up next\n", game.playerone);
+     }
+
+    while (!valid)
+    {
+      printf("Would you like to play or hold  ? [P|H]");
+      scanf("%c", &userinput);
+
+      if (userinput == 'h' || userinput == 'H')
+      {
+        printf("%s has chosen to hold !\n");
+        game.turn='W';
+      }
+        else if (userinput == 'p' || userinput == 'P')
+        {
+          while (!found)
+          {
+            printf("Please enter an x coordinate (letter A-H)");
+            scanf("%c", &xcoordinate);
+            if (userinput >= 65 || userinput <= 72 )
+            {
+              userinput -= 32;
+              found=true;
+            }
+              else if ( (userinput < 65 && userinput > 72) && (userinput > 97 && userinput < 122) )
+              {
+                found=true;
+              }
+                else
+                {
+                  printf("Invalid input. Please enter a letter A-H\n\n");
+                }
+          }
+          found=false;
+          while (!found)
+          {
+            printf("Please enter a y coordinate (number 1-8)");
+            scanf("%d", &ycoordinate);
+
+            if ( ycoordinate >= 1 && ycoordinate <= 8 )
+            {
+              found=true;
+            }
+              else
+              {
+                printf("Invalid input. Please enter a number 1-8\n\n");
+              }
+          }
+          xconverter(xcoordinate, xcoordinateconverted);
+          movechecker(game, xcoordinateconverted, ycoordinate);
+        }
+          else
+          {
+            printf("Invalid input. Please enter the letter P to Pass or H to Hold\n\n");
+          }
+    }
+  }
+    else if (game.turn == 'W')
+}
+
+int xconverter(char xcoordinate, int xcoordinateconverted)
+{
+  char converterstring[8]={a,b,c,d,e,f,g,h};
+  int i=0;
+  bool found=false;
+
+  while (!found)
+  {
+    if (xcoordinate == converterstring[i])
+    {
+      xcoordinateconverted=i;
+      xcoordinateconverted++;
+      found=true
+    }
+    i++;
+  }
+}
+
+int movechecker(struct board game, char xcoordinateconverted, int ycoordinate)
+{
+  int i=0;
+  int j=0;
+  int k=0;
+  bool found=false;
+  int count=0;
+
+  if (boardgame[i][xcoordinateconverted]='-')
+  {
+    count += 10;
+  }
+
+  for (i=0;i<8;i++)
+  {
+    if (game.turn == boardgame[i][xcoordinateconverted])
+    {
+      
+      count++;
+      //vertical
+    }
+  }
+
+  for (j=0;j<8;j++)
+  {
+    if (game.turn == boardgame[ycoordinate][j])
+    {
+      count++;
+      //horizontal
+    }
+  }
+
+  //bottom-right diagonal
+  for (i=ycoordinate+1;i<8;i++)
+  {
+    for (j=xcoordinateconverted+1;j<8;j++)
+    {
+      if (game.turn == boardgame[i][j])
+      {
+        count++;
+      }
+    }
+  }
+
+  //bottom-left diagonal
+  for (i=ycoordinate+1;i<8;i++)
+  {
+    for (j=xcoordinateconverted-1;j<8;j--)
+    {
+      if (game.turn == boardgame[i][j])
+      {
+        count++;
+      }
+    }
+  }
+
+  //top-left diagonal
+  for (i=ycoordinate-1;i<8;i--)
+  {
+    for (j=xcoordinateconverted-1;j<8;j--)
+    {
+      if (game.turn == boardgame[i][j])
+      {
+        count++;
+      }
+    }
+  }
+
+  //top-right diagonal
+  for (i=ycoordinate-1;i<8;i--)
+  {
+    for (j=xcoordinateconverted+1;j<8;j++)
+    {
+      if (game.turn == boardgame[i][j])
+      {
+        count++;
+      }
+    }
+  }
 }
